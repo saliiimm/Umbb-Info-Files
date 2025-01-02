c4dc11d143a7cc82c0fcdedb8c1384b4c4e3210d
Ghalem Salim <salimghalem40@gmail.com>
2025-01-02 14:19:16 +0100

    footer and navbar responsive done

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,19 +2,33 @@ import './NavBar.css';
 import { Button } from 'antd';
 import { MdNoteAdd } from 'react-icons/md';
 import { HashLink } from 'react-router-hash-link';
+import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const scrollWithOffset = (el: any) => {
     const yOffset = -70;
     const y = el.getBoundingClientRect().top + window.scrollY + yOffset;
     window.scrollTo({ top: y, behavior: 'smooth' });
+    setIsMenuOpen(false);
   };
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <nav>
       <HashLink to="/#home" scroll={scrollWithOffset}>
         <h1>UMBB Info Files</h1>
       </HashLink>
-      <ul>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <FaBars size={30} />
+      </div>
+
+      <ul className={isMenuOpen ? 'active' : ''}>
         <li>
           <HashLink to="/#1st-year" scroll={scrollWithOffset}>
             1st Year
@@ -44,12 +58,24 @@ const NavBar = () => {
             5th Year
           </HashLink>
         </li>
+        {isMenuOpen && (
+          <Button
+            type="primary"
+            icon={<MdNoteAdd size={18} />}
+            disabled
+            title="soon available"
+          >
+            Contribute
+          </Button>
+        )}
       </ul>
+
       <Button
         type="primary"
         icon={<MdNoteAdd size={18} />}
         disabled
         title="soon available"
+        className="nav-button"
       >
         Contribute
       </Button>
