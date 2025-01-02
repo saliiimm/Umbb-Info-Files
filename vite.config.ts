5ec9ac06767469989acffbef8a57cb4d1cef9c00
Ghalem Salim <salimghalem40@gmail.com>
2025-01-02 14:31:39 +0100

    Optimisation & SEO

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      svgo: { plugins: [{ name: 'removeViewBox' }] },
+    }),
+  ],
   build: {
-    sourcemap: true,
+    minify: 'esbuild',
+    sourcemap: false,
   },
   resolve: {
     alias: {
